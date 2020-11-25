b2adb03508711427b8b82537271d09e9064ce177
Cjunior <cjunior.network@gmail.com>
2020-11-24 22:19:12 -0300

    challenge complete

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acumulator: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          acumulator.income += transaction.value;
+        } else {
+          acumulator.outcome += transaction.value;
+        }
+
+        return acumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    ) as Balance;
+
+    const balance = { income, outcome, total: income - outcome };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ type, value, title }: Request): Transaction {
+    const transaction = new Transaction({ type, value, title });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
